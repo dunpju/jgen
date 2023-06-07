ba23b87e8532204866af3ac48bc67a6fab733a7f
dengpju <dengpju@163.com>
2023-06-07 18:13:06 +0800

    更新

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,12 @@
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import com.dunpju.gen.Gen;
 import com.yumi.enums.EnumYesOrNo;
-import com.yumi.jerrcode.ErrCode;
 
+import java.sql.Types;
+import java.util.Collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,5 +34,32 @@ public class Test {
                 "D:\\php\\jgen\\src\\test\\java\\com\\yumi\\enums");
         System.out.println(EnumYesOrNo.NO.getCode());
 
+        FastAutoGenerator.create("jdbc:mysql://192.168.8.99/test?characterEncoding=UTF-8", "root", "1qaz2wsx")
+                .globalConfig(builder -> {
+                    builder.author("ffff") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir("D:\\php\\jgen\\src\\test\\java\\com\\yumi\\db"); // 指定输出目录
+                })
+                .dataSourceConfig(builder -> builder.typeConvertHandler((globalConfig, typeRegistry, metaInfo) -> {
+                    int typeCode = metaInfo.getJdbcType().TYPE_CODE;
+                    if (typeCode == Types.SMALLINT) {
+                        // 自定义类型转换
+                        return DbColumnType.INTEGER;
+                    }
+                    return typeRegistry.getColumnType(metaInfo);
+
+                }))
+                .packageConfig(builder -> {
+                    builder.parent("com.baomidou.mybatisplus.samples.generator") // 设置父包名
+                            .moduleName("system") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D:\\php\\jgen\\src\\test\\java\\com\\yumi\\db")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("ts_user") // 设置需要生成的表名
+                            .addTablePrefix("ts_", "c_"); // 设置过滤表前缀
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
     }
 }
