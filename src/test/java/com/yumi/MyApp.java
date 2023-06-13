ebdaddb055a7c3c9e2cff329c22fd35a6e056d87
dengpju <dengpju@163.com>
2023-06-13 13:56:12 +0800

    更新

--- src/test/java/com/yumi/MyApp.java
+++ src/test/java/com/yumi/MyApp.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAutoConfiguration
 @ComponentScan// 自动扫描根包下的所有
